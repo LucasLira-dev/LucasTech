ab82bdf99b4e8329f0a0d07d22ca29e1ede65e42
lucas mendes <lucasmndes.dev@gmail.com>
2024-12-25 13:25:31 -0300

    usando promise para deixar o envio do formulario mais legal

--- script.js
+++ script.js
@@ -7,3 +7,48 @@ botao.addEventListener("click", (evt)=>{
 
     input.focus()
 })
+
+
+const btn_contato= document.getElementById("cont")
+const nome=  document.getElementById("nome")
+const email=  document.getElementById("email")
+const assunto=  document.getElementById("assunto")
+
+
+btn_contato.addEventListener("click", (evt)=>{
+    evt.preventDefault()
+    btn_contato.innerHTML= "Enviando....."
+
+    promessa()
+    .then((retorno)=>{
+        btn_contato.innerHTML= retorno
+    })
+
+    .catch((retorno)=>{
+        btn_contato.innerHTML= retorno
+    })
+})
+
+
+const promessa=()=>{
+    let promise= new Promise((resolve, reject)=>{
+       
+        let tempo= 3000
+
+        setTimeout(()=>{
+            if(nome.value!="" && email.value!="" && assunto.value!="" ){
+                resolve("Mensagem enviada!")
+            } else{
+                reject("Erro, tente novamente.")
+            }
+        }, tempo)
+    })
+
+    promise.then((retorno)=>{
+        btn_contato.innerHTML= retorno
+    })
+
+    promise.catch((retorno)=>{
+        btn_contato.innerHTML= retorno
+    })
+}
